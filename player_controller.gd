fe33acc01cef379d716017f167d67bc111424528
Anthony Mortimer <agm20@outlook.com>
2023-12-14 11:24:35 -0500

    Initial commit

---
+++ player_controller.gd
@@ -0,0 +1,12 @@
+extends RigidBody2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+func _process(delta):
+	pass
+
+func _physics_process(delta):
+	apply_force(Vector2(10, 0))
