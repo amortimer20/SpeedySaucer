2ccff073bcf52b3ccd91530e2a9455748e8ea23c
Anthony Mortimer <anthonymortimer@Anthonys-iMac.local>
2023-12-15 16:06:16 -0500

    finish collisions

--- scripts/level.gd
+++ scripts/level.gd
@@ -4,3 +4,7 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	print("Hello")
+
+
+func _on_maze_body_exited(body):
+	get_tree().reload_current_scene()
