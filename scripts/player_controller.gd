020a757f72e761efe19328e75a966e5fb0141c86
Anthony Mortimer <anthonymortimer@Anthonys-iMac.local>
2023-12-15 13:41:39 -0500

    Add player input

--- scripts/player_controller.gd
+++ scripts/player_controller.gd
@@ -1,12 +1,14 @@
 extends RigidBody2D
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass
-
-func _process(delta):
-	pass
+var force := 1000
 
 func _physics_process(delta):
-	apply_force(Vector2(10, 0))
+	if Input.is_action_pressed("move_up"):
+		apply_force(Vector2(0, -force))
+	if Input.is_action_pressed("move_right"):
+		apply_force(Vector2(force, 0))
+	if Input.is_action_pressed("move_down"):
+		apply_force(Vector2(0, force))
+	if Input.is_action_pressed("move_left"):
+		apply_force(Vector2(-force, 0))
+	
